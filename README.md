56bdde3707bf6f36f8a60f44f453b500d3a4fb9a
Mike Pagel <mike@mpagel.de>
2015-12-16 11:16:59 +0100

    Prepared integration in Travis CI.

---
+++ README.md
@@ -0,0 +1,40 @@
+# jep-cmake [![Build Status](https://travis-ci.org/jep-project/jep_cmake.svg?branch=master)](https://travis-ci.org/jep-project/jep_cmake)
+
+JEP-based CMake support in the making.
+
+Once completed, this is a command line application implementing a JEP backend for CMake. It enables editors that have JEP-plugin installed to
+improve their CMake editing support beyong pure syntax highlighting.
+
+## Current state and features
+
+* Parser for CMake files.
+* Builds dictionary of user functions and macros.
+
+## Upcoming features and feature ideas
+
+* Code completion of commands (built-in and user functions and macros), first per currently edited file.
+* Code completion for all files in a project, respecting CMake scope rules and module imports.
+* Code completion for variables (respecting scope rules).
+* Code completion for targets.
+* Go to definition of commands, variables, targets.
+* Error annotations.
+* Renaming of user commands, variables, targets.
+
+## Installation
+
+From a Python 3.3+ environment install package from Github (will be uploaded to PyPI when more mature):
+
+    > pip install https://github.com/jep-project/jep-cmake/archive/master.zip
+    
+After installation, the Python environment has a new command `jep-cmake`, that will run the backend.
+
+## Usage
+
+See the [JEP protocol|https://github.com/jep-project/jep/blob/master/protocol.md] for detailed instruction how to configure JEP via a
+`.jep` file. In the most simple situation (`jep-cmake` in path, ...) the following `.jep` file in the root folder of your project that
+contains CMake files should be enough:
+
+    *.cmake,CMakeLists.txt:
+    jep-cmake
+
+Currently `jep-cmake` does not yet take any command line arguments.
---
+++ setup.py
@@ -0,0 +1,59 @@
+"""Setup script for jep.python."""
+
+
+def main():
+    """Wrapper around imports to prevent them being executed immediately upon this module being only imported (Sublime plugin workaround)."""
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test as TestCommand
+    import sys
+
+    class PyTestCommand(TestCommand):
+        user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+        def initialize_options(self):
+            TestCommand.initialize_options(self)
+            self.pytest_args = []
+
+        def finalize_options(self):
+            TestCommand.finalize_options(self)
+            self.test_args = []
+            self.test_suite = True
+
+        def run_tests(self):
+            #import here, cause outside the eggs aren't loaded
+            import pytest
+
+            errno = pytest.main(self.pytest_args)
+            sys.exit(errno)
+
+    # configure dependencies corresponding to interpreter version:
+    install_requires = [
+        'antlr4-python3-runtime',
+        'u-msgpack-python',
+    ]
+
+    if sys.version_info < (3, 3):
+        print('This Python version is not supported, minimal version 3.3 is required.')
+        sys.exit(1)
+    if sys.version_info < (3, 4):
+        install_requires.append('enum34')
+setup(
+        name='jep-cmake',
+        version='0.0.1',
+        packages=find_packages(),
+        install_requires=install_requires,
+        tests_require=[
+            'pytest'
+        ],
+        cmdclass={'test': PyTestCommand},
+        url='https://github.com/jep-project/jep-cmake',
+        license='',
+        author='Mike Pagel',
+        author_email='mike@mpagel.de',
+        description='CMake backend for JEP-enabled editors, see http://joint-editors.org/.'
+)
+
+
+
+if __name__ == '__main__':
+    main()
